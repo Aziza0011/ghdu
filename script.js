dbbb2c2f059c33bc56fc7e821a2422556166f686
Aziza0011 <khakimovaaziza95@gmail.com>
2024-07-06 11:39:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let banan =document.querySelector('h1')
+let olma =document.querySelector('body')
+let c =prompt('Textni rangini kiriting ')
+let d =prompt('Backroundni rangini kiriting ')
+
+olma.style.background = d
+banan.style.color = c
+
+
+
